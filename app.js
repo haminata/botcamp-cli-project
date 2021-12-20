16e6ad82da017661085c91b81647da3b0aeaf2b1
anthony <anthony.chinwo@gmail.com>
2021-12-20 02:07:14 +0000

    - switched to reading movies data from json db

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ const readlineSync = require('readline-sync');
 const API = require('./lib/API');
 
 function displayMovies(){}
-function chooseAMovieProperty(movieProperty){}
-
-
+function displayLocationsForMovieSelected(){}
+function movieLocation(choice){
+  const movies = API.read("movies");
+  
+}
 
 function displayCinemaProperties(cinemaProperty) {
   cinemaProperty.forEach((elementProperty, index) =>{
     console.log(`${index + 1}. ${elementProperty}`);
   });
+  const choice = readlineSync.question("Please choose an option: ");
+  renderUserChoice(choice, cinemaProperty);
 }
 
-function displayLocationsForMovieSelected(){}
+function renderUserChoice(choice, cinemaProperty) {
+  const choiceAsNumber = parseInt(choice);
+  const chosenNumber = cinemaProperty[choiceAsNumber -1];
+  
+  if(chosenNumber !== undefined){
+    console.log(`You selected ${chosenNumber}`);
+  }else {
+    console.log("We don't have a cinema in this location yet!")
+    displayCinemaProperties(cinemaProperty);
+  }
+}
+
+
 
 function mainMenu() {
   console.log("----------------");
@@ -32,10 +48,9 @@ function mainMenu() {
     // get all books
     const movies = API.read("movies");
     displayCinemaProperties(movies.map(element => element.title));
-    choice;
 
     // return to main menu
-    mainMenu();
+    // mainMenu();
   } else if (choice === "2") {
     console.log("-----------------");
     console.log("- CHOOSE A LOCATIONS -");
